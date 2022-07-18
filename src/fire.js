7f68cb6b4018c9c90353269729efd9c76b78ea97
Michael Salamon <msalamo000@citymail.cuny.edu>
2022-07-17 21:25:25 -0400

    Connected app to firebase project

---
+++ src/fire.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const get_fire_cred=require('./fire_cred.js');
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: get_fire_cred.api,
+  authDomain: get_fire_cred.dom,
+  projectId: get_fire_cred.projId,
+  storageBucket: get_fire_cred.bucket,
+  messagingSenderId: get_fire_cred.senderId,
+  appId: get_fire_cred.appId,
+  measurementId: get_fire_cred.msrmentId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

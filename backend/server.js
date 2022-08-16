23aeace5710be951749a020cd810d9bdf539bb8b
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-08-16 09:34:28 -0400

    Implemented getDatabase() to send a GET request to Notion API for database metadata #3

--- backend/server.js
+++ backend/server.js
@@ -4,34 +4,63 @@ const cors = require('cors');
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 
-const app = express();
+// const app = express();
 
 // suppress GitGuardian alert by placing Notion tokens
 // in a seperate .js file
 const get_notion_tokens=require('./notion_tokens.js');
 
 // avoid cors errors
-app.use(cors());
+// app.use(cors());
 
-const PORT = 4000;
-const HOST = "localhost";
+// const PORT = 4000;
+// const HOST = "localhost";
 
 // integration token
 const notion = new Client({ auth : get_notion_tokens.intgr_token});
 // database token
 const databaseId = get_notion_tokens.page_token;
 
+// Sends GET request for Notion database meta-data
+const getDatabase = async () => {
+    try {
+        // 15 element json
+        const response = await notion.databases.retrieve({ database_id: databaseId });
+
+        // view entire json
+        // console.log(response);
+
+        // 4 relevant elements
+        const responseResults = 
+            {
+            db_name: response.title[0].plain_text,
+            is_inline: response.is_inline,
+            // ISO 8601 time -> Date object -> String
+            db_init_time: new Date(response.created_time).toString('en', {timezone: 'EST'}),
+            db_last_edit_time: new Date(response.last_edited_time).toString('en', {timezone: 'EST'})
+            };
+        
+        console.log(responseResults);
+        return responseResults;
+    }
+    catch(error) {
+        console.log(error);
+    }
+  };
+getDatabase();
+
 // POST request
 // POST name, phoneNumber, extraInfo
 // Functionality: Make a database entry in a Notion page w/ the databaseId above
 // localhost:4000/submitFormNotion
-app.post('/submitFormToNotion', jsonParser, async (req, res) => {
+// app.post('/submitFormToNotion', jsonParser, async (req, res) => {
     // req.body
     /*{
         name: "Michael Salamon",
         phoneNumber: "Michael Salamon",
         extraInfo: "Michael Salamon"
     }*/
+/*
     const name = req.body.name;
     const phoneNumber = req.body.phoneNumber;
     const extraInfo = req.body.extraInfo;
@@ -75,16 +104,14 @@ app.post('/submitFormToNotion', jsonParser, async (req, res) => {
     catch(error) {
         console.log(error);
     }
-});
+});*/
 
 // To-do:
 // 1. Change POST request to contain hard-coded data
 // 2. Make a GET request that extracts relevant data
 
-
+/*
 app.listen(PORT, HOST, () => {
     console.log("Starting proxy at " + HOST + ":" + PORT); // localhost:4000
-});
-
-
+});*/
 

ed22ef3261ca275149d92b49b121d78fce518cef
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-08-16 14:06:01 -0400

    Implemented function writeUserQueAns() which submits data to Firebase Realtime #2

--- src/fire.js
+++ src/fire.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app'
+import {getDatabase, ref, set} from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +22,33 @@ const firebaseConfig = {
 const fire_app = initializeApp(firebaseConfig);
 
 export default fire_app;
+
+// POST to Firebase Realtime
+function writeUserQueAns (useremail, allQueAns) {
+  const db = getDatabase();
+  // users -> userId -> questionnaire
+  const reference = ref(db, 'users/' + useremail + '/questionnaire');
+  // questionnaire -> question_1 : answer_1...
+  set(reference, allQueAns);
+}
+
+// GET to Firebase Realtime
+
+
+// example questionnaire + responses
+const example_ques={
+    que_ans_1: {
+        que : "How is your organizational ability?",
+        ans : "Terrible"
+    },
+    que_ans_2: {
+        que : "What times of day do you usually get work done?",
+        ans : "10AM-5PM EST"
+    },
+    que_ans_3: {
+        que : "What days of the week are you most productive?",
+        ans : "Mon, Tue, and Wed"
+    }
+}
+
+writeUserQueAns("michaelsalamon78", example_ques);

04d3304c991f894bdda664f0143dec5df49efe24
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-08-16 11:11:42 -0400

    Implemented updateDatabase() to send a POST request to Notion API to add a new page #3

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,5 @@
-const express = require('express');
 const { Client } = require('@notionhq/client');
-const cors = require('cors');
 var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
 
 // const app = express();
 
@@ -10,18 +7,12 @@ var jsonParser = bodyParser.json()
 // in a seperate .js file
 const get_notion_tokens=require('./notion_tokens.js');
 
-// avoid cors errors
-// app.use(cors());
-
-// const PORT = 4000;
-// const HOST = "localhost";
-
 // integration token
 const notion = new Client({ auth : get_notion_tokens.intgr_token});
 // database token
 const databaseId = get_notion_tokens.page_token;
 
-// Sends GET request for Notion database meta-data
+// Sends GET request to Notion database for metadata
 const getDatabase = async () => {
     try {
         // 15 element json
@@ -46,25 +37,24 @@ const getDatabase = async () => {
     catch(error) {
         console.log(error);
     }
-  };
+};
 getDatabase();
 
-// POST request
-// POST name, phoneNumber, extraInfo
-// Functionality: Make a database entry in a Notion page w/ the databaseId above
-// localhost:4000/submitFormNotion
-// app.post('/submitFormToNotion', jsonParser, async (req, res) => {
-    // req.body
-    /*{
-        name: "Michael Salamon",
-        phoneNumber: "Michael Salamon",
-        extraInfo: "Michael Salamon"
-    }*/
-/*
-    const name = req.body.name;
-    const phoneNumber = req.body.phoneNumber;
-    const extraInfo = req.body.extraInfo;
+// TO-DO: Obtain row data from database
+// I think you need to use notion.databases.query
+/*const queryDatabase = async () => {
+    try {
+        const response = await notion.databases.query({ database_id: databaseId });
 
+        console.log(response)
+    }
+    catch(error) {
+        console.log(error);
+    }
+};*/
+
+// Sends a POST request to Notion database to create a new page
+const updateDatabase = async () => { 
     try {
         const response = await notion.pages.create({
             parent: { database_id: databaseId },
@@ -73,7 +63,7 @@ getDatabase();
                     title: [
                         {
                             text: {
-                                content: name
+                                content: "Jay"
                             }
                         }
                     ]
@@ -82,7 +72,7 @@ getDatabase();
                     rich_text: [
                         {
                             text: {
-                                content: phoneNumber
+                                content: "333-222-1111"
                             }
                         }
                     ]
@@ -91,27 +81,17 @@ getDatabase();
                     rich_text: [
                         {
                             text: {
-                                content: extraInfo
+                                content: "I like to design on Figma"
                             }
                         }
                     ]
                 }
             }
-        })
+        });
         console.log(response);
-        console.log("SUCCESS!")
     }
     catch(error) {
         console.log(error);
     }
-});*/
-
-// To-do:
-// 1. Change POST request to contain hard-coded data
-// 2. Make a GET request that extracts relevant data
-
-/*
-app.listen(PORT, HOST, () => {
-    console.log("Starting proxy at " + HOST + ":" + PORT); // localhost:4000
-});*/
-
+};
+updateDatabase();

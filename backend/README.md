19b582942eff76f871819d7a5f15430f99722a57
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-09-16 12:55:04 -0400

    Updated Notion API README.md on setting configurations and token values.

--- backend/README.md
+++ backend/README.md
@@ -6,22 +6,33 @@ The purpose of the backend folder is to interact w/ the Notion JS SDK.
 
 1. Create a new Notion Workspace for testing.
 2. Create a new integration: https://www.notion.so/my-integrations (Select the testing workspace)
-3. Upon submission, locate the **Internal Integration token_id**. Note this value.
-4. Create a new page in your Workspace. Set up an inline table database. Hover over the database 
+3. Under 'Capabilities', make sure to check 'Read comments' and 'Insert Comments' (unchecked by default.)
+4. Upon submission, locate the **Internal Integration token_id**. Note this value.
+5. Create a new page in your Workspace. Set up an inline table database. Hover over the database 
 and click 'Open as full page'.
-5. Analyze the unique Notion link associated with this page. It should look something like the example below. Note the value after *notion.so/* but before *?v=*. This is your **Database token_id**.
+6. Analyze the unique Notion link associated with this page. It should look something like the example below. Note the value after *notion.so/* but before *?v=*. This is your **page_token**.
 >notion.so/a8958c89072c8927e8910271?v=cea289893048a89387c890ee
 
 
 ## Setup
-1. Under the selected page in your Workspace, under the Share options, press "Invite" and select your newly created integration.
+1. Under the selected page in your Workspace, under the Share options, press "Invite" and select your integration. You may use the same, or different, integrations across multiple pages. 
 2. The page is now ready for API commands!  
 
 ## Next Steps
 Create a **notion_tokens.js** file w/ the following format:  
 ```
 module.exports = {
-    intgr_token: "Internal_Integration_token_value",
-    page_token: "Database_token_value",
-  };
+  template: {
+    tokens: {
+      intgr_token: "Internal_Integration_token_value",
+      page_token: "Templ_token_value"
+    }
+  },
+  project: {
+    tokens: {
+      intgr_token: "Internal_Integration_token_value",
+      page_token: "Proj_token_value"
+    }
+  }
+};
 ```
--- backend/notionAPI.js
+++ backend/notionAPI.js
@@ -1,35 +1,31 @@
 const { Client } = require('@notionhq/client');
-var bodyParser = require('body-parser')
 
-// const app = express();
-
-// suppress GitGuardian alert by placing Notion tokens
-// in a seperate .js file
 const get_notion_tokens=require('./notion_tokens.js');
 
 // integration token
 const notion = new Client({ auth : get_notion_tokens.intgr_token});
-// database token
-const databaseId = get_notion_tokens.page_token;
+// page tokens
+const templateId = get_notion_tokens.templ_token;
+const projId = get_notion_tokens.proj_token;
 
-// Sends GET request to Notion database for metadata
-const getDatabase = async () => {
-    try {
+// Collects user activity on curated DailyNotion template space
+const getTemplate = async (templateId) => {
+    try 
+    {
         // 15 element json
-        const response = await notion.databases.retrieve({ database_id: databaseId });
+        const response = await notion.databases.retrieve({ database_id: templateId });
 
         // view entire json
         // console.log(response);
 
         // 4 relevant elements
-        const responseResults = 
-            {
+        const responseResults = {
             db_name: response.title[0].plain_text,
             is_inline: response.is_inline,
             // ISO 8601 time -> Date object -> String
             db_init_time: new Date(response.created_time).toString('en', {timezone: 'EST'}),
             db_last_edit_time: new Date(response.last_edited_time).toString('en', {timezone: 'EST'})
-            };
+        };
         
         console.log(responseResults);
         return responseResults;
@@ -38,26 +34,13 @@ const getDatabase = async () => {
         console.log(error);
     }
 };
-getDatabase();
+getTemplate();
 
-// TO-DO: Obtain row data from database
-// I think you need to use notion.databases.query
-/*const queryDatabase = async () => {
-    try {
-        const response = await notion.databases.query({ database_id: databaseId });
-
-        console.log(response)
-    }
-    catch(error) {
-        console.log(error);
-    }
-};*/
-
-// Sends a POST request to Notion database to create a new page
-const updateDatabase = async () => { 
+// Adds a new entry to DailyNotion template space
+const updateTemplate = async (templateId) => { 
     try {
         const response = await notion.pages.create({
-            parent: { database_id: databaseId },
+            parent: { database_id: templateId },
             properties: {
                 Name: {
                     title: [
@@ -88,10 +71,15 @@ const updateDatabase = async () => {
                 }
             }
         });
-        console.log(response);
+        return response;
     }
     catch(error) {
-        console.log(error);
+        return error;
     }
 };
-updateDatabase();
+// updateTemplate();
+
+// Adds a new comment to user project space
+const msgProject = async () => {
+
+};

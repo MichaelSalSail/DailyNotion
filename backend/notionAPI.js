9d2c5795ac8783ad4d62b3557766a1b2deab18af
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-09-16 13:33:10 -0400

    Adjusted Notion API function template name and parameters

--- backend/notionAPI.js
+++ backend/notionAPI.js
@@ -1,23 +1,20 @@
 const { Client } = require('@notionhq/client');
 
-const get_notion_tokens=require('./notion_tokens.js');
+const notion_info=require('./notion_tokens.js');
 
-// integration token
-const notion = new Client({ auth : get_notion_tokens.intgr_token});
-// page tokens
-const templateId = get_notion_tokens.templ_token;
-const projId = get_notion_tokens.proj_token;
+// Access template page
+const templateIntgr = new Client({ auth : notion_info.template.tokens.intgr_token});
+const templateId = notion_info.template.tokens.page_token;
+// Access project page
+const projIntgr = new Client({ auth : notion_info.project.tokens.intgr_token});
+const projId = notion_info.project.tokens.page_token;
 
 // Collects user activity on curated DailyNotion template space
-const getTemplate = async (templateId) => {
+const getTemplate = async (templateIntgr, templateId) => {
     try 
     {
         // 15 element json
-        const response = await notion.databases.retrieve({ database_id: templateId });
-
-        // view entire json
-        // console.log(response);
-
+        const response = await templateIntgr.databases.retrieve({ database_id: templateId });
         // 4 relevant elements
         const responseResults = {
             db_name: response.title[0].plain_text,
@@ -26,58 +23,79 @@ const getTemplate = async (templateId) => {
             db_init_time: new Date(response.created_time).toString('en', {timezone: 'EST'}),
             db_last_edit_time: new Date(response.last_edited_time).toString('en', {timezone: 'EST'})
         };
-        
-        console.log(responseResults);
-        return responseResults;
+        console.log("getTemplate()", responseResults);
     }
     catch(error) {
-        console.log(error);
+        console.log("getTemplate()", error);
     }
 };
-getTemplate();
+getTemplate(templateIntgr,templateId);
 
 // Adds a new entry to DailyNotion template space
-const updateTemplate = async (templateId) => { 
+const updateTemplate = async (templateIntgr, templateId, templText) => { 
     try {
-        const response = await notion.pages.create({
+        const response = await templateIntgr.pages.create({
             parent: { database_id: templateId },
             properties: {
-                Name: {
+                Week: {
                     title: [
                         {
                             text: {
-                                content: "Jay"
+                                content: templText[0]
                             }
                         }
                     ]
                 },
-                "Phone Number": {
+                "Question 1": {
                     rich_text: [
                         {
                             text: {
-                                content: "333-222-1111"
+                                content: templText[1]
                             }
                         }
                     ]
                 },
-                "Extra Information": {
+                "Question 2": {
                     rich_text: [
                         {
                             text: {
-                                content: "I like to design on Figma"
+                                content: templText[2]
+                            }
+                        }
+                    ]
+                },
+                "Question 3": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: templText[3]
+                            }
+                        }
+                    ]
+                },
+                "Question 4": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: templText[4]
                             }
                         }
                     ]
                 }
             }
         });
-        return response;
+        console.log("updateTemplate()", response);
     }
     catch(error) {
-        return error;
+        console.log("updateTemplate()", error);
     }
 };
-// updateTemplate();
+let templText=["1",
+               "[What is 1 thing you are grateful for today?]",
+               "[What is your most recent success? How proud are you from a scale of 1-10?]",
+               "[What excites you about your current project?]",
+               "[Write down 1 reason you should feel confident working on your project this week.]"]
+updateTemplate(templateIntgr,templateId, templText);
 
 // Adds a new comment to user project space
 const msgProject = async () => {
